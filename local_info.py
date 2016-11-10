f2b15dea64c05040c214f46be3497b608d9640b3
jamieconn <ec2-user@ip-172-31-61-104.ec2.internal>
2016-11-10 15:45:44 -0500

    updating ftp function

---
+++ local_info.py
@@ -0,0 +1,5 @@
+ftp_pass = 'escc2902'
+ftp_user = 'a8506764'
+#ftp_url = 'isthedartrunning.co.uk'
+ftp_dir = 'public_html' # SET TO None if wanting file to go to top level
+ftp_url = '31.170.160.87'
