0d71e8f2587a23b845a5db4c74ea00367c9275c5
Jamie Conn <jamie.conn65@gmail.com>
2017-02-06 23:07:55 +0000

    Removed excess code

--- html/graphScript.js
+++ html/graphScript.js
@@ -1,22 +1,5 @@
 google.load("visualization", "1", {packages:["corechart"]});
 google.setOnLoadCallback(drawChart);
-function drawChart1() {
-      var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-              ['2004',  1000,      400],
-                  ['2005',  1170,      460],
-                      ['2006',  660,       1120],
-                          ['2007',  1030,      540]
-                            ]);
-
-        var options = {
-                title: 'Company Performance',
-                    hAxis: {title: 'Year', titleTextStyle: {color: 'red'}}
-                     };
-
-                     var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-                       chart.draw(data, options);
-}
 
 
 
