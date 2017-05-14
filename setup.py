39856727b76378a07865b7862dde0f5bc15bcee8
root <jamie.conn65@gmail.com>
2017-05-14 12:56:03 +0000

    add IF EXISTS to sql command

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ import sqlite3 as lite
 def create_db(river, database):
     con = lite.connect(database)
     cur = con.cursor()
-    cur.execute("CREATE TABLE {river}(Id INTEGER PRIMARY KEY, timestamp TIMESTAMP UNIQUE, rain FLOAT, level FLOAT, predict FLOAT)".format(river = river))
+    cur.execute("CREATE TABLE IF NOT EXISTS {river}(Id INTEGER PRIMARY KEY, timestamp TIMESTAMP UNIQUE, rain FLOAT, level FLOAT, predict FLOAT, forecast FLOAT)".format(river = river))
     con.commit()
     con.close()
 
 
 if __name__ == "__main__":
     create_db('dart', 'data.db')
+    create_db('nevis', 'data.db')
