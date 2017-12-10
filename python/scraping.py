2d099473acf44e8f0a08479cd3d158b2fe0a47c3
Jamie Conn <jconn@Jamies-MBP.home>
2017-12-10 10:44:23 +0000

    refactored dartcom code

--- python/scraping.py
+++ python/scraping.py
@@ -1,6 +1,5 @@
 import datetime
 import numpy as np
-from scipy import misc
 import glob, os
 import math
 import urllib2
@@ -20,8 +19,11 @@ import sqlite3 as lite
 import sys
 import os.path
 
-from scipy import sum, average
+from scipy import sum, average, misc
 fdir = os.path.abspath(os.path.dirname(__file__))
+
+
+from dartcom import get_rainfall
 
 river = 'dart'
 database = os.path.join(fdir, '../data.db')
@@ -62,49 +64,6 @@ black = misc.imread(os.path.join(fdir, '../ref_images/black.png'))
 numbers = [zero, one, two, three, four, five, six, seven, eight, nine, dot, black]
 
 
-def return_digit(image, dig):
-    n = 4 + dig * 12
-    return image[25:45, n:n+12]
-
-def to_grayscale(arr):
-    if len(arr.shape) == 3:
-        return average(arr, -1)
-    else:   
-        return arr
-
-def match_digit(arr1, arr2):
-    diff = arr2 - arr1
-    return sum(abs(diff))
-
-# Returns the position of the best match out of the possible images definited in numbers
-def digit_to_value(arr):
-    mini = 80000
-    for i in range(0, len(numbers)):
-        dum = match_digit(arr, to_grayscale(numbers[i]))
-        if dum < mini:
-            mini = dum
-            result = i
-    return result
-
-def image_to_value(im, limit):
-    string = ''
-    for i in range(0, limit):
-        dig = digit_to_value(return_digit(im, i))
-        if dig < 10:
-            string += str(dig)
-        elif dig == 10:
-            string += "."
-        elif dig == 11: # case where digit is black image
-            return string
-    return string
-
-def get_rainfall():
-    url = "http://www.dartcom.co.uk/images/weather/vws1003.jpg"
-
-    file = cStringIO.StringIO(urllib2.urlopen(url).read())
-    im = misc.imread(file)
-    im = to_grayscale(im)
-    return float(image_to_value(im, 6))
 
 
 def update_sql_rain(time_val, rain_val):
