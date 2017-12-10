b241b829edf0cbf6ed1a9ad09c1b16326f8adb35
Jamie Conn <jconn@Jamies-MBP.home>
2017-12-10 20:39:18 +0000

    Added logging message

--- python/modelLib.py
+++ python/modelLib.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import sqlite3
 import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from logfuncts import logger
 
 FDIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE_PATH = os.path.join(FDIR, '../data.db')
@@ -11,7 +11,7 @@ DATABASE_PATH = os.path.join(FDIR, '../data.db')
 def load_dataframe_from_sql(river, limit=-1):
     """Load data from the database and return a pandas dataframe. 
     Limit param specifies number of rows returned. Default is to return all"""
-    
+    logger.debug("loading df for river {river} from sql with row limit of {limit}".format(river=river, limit=limit))
     con = sqlite3.connect(DATABASE_PATH)
     cur = con.cursor()
     query = """
