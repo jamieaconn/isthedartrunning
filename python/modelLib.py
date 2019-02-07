ef680880d4805a712f8f3d3a1fba4881e7847fd2
Jamie Conn <jamie.conn65@gmail.com>
2019-02-07 21:14:25 +0000

    Rewrite production code to use rnn model

--- python/modelLib.py
+++ python/modelLib.py
@@ -9,6 +9,7 @@ import numpy as np
 import requests
 import sqlite3
 import boto3
+import tensorflow as tf
 
 # local modules
 from logfuncts import logger
@@ -60,170 +61,110 @@ def load_dataframe_from_sql(river, limit=-1):
 
     return df
 
-def preprocessing(df):
-    """Reindex to include missing timestamps and create new column for actual rain from cumulative rain"""
-    logger.debug("Fill in missing timestamps by reindexing")
-    
+def rnn_model(df):
+    # Reindex to include missing timestamps and create new column for actual rain from cumulative rain
+
+    num_steps = 80
+    num_level_updates = 40
+
+    current_time = time.time()
+    current_time = pd.to_datetime(current_time - (current_time % (15*60)), unit='s')
+    latest_level_update_timestamp = max(df[df.level.notnull()].index)
+    latest_rain_time = max(df.index[df.cum_rain.notnull()])
+    logger.info('latest rain update at: ' + str(latest_rain_time))
+
+    # Remove rows after latest cum_rain value (no longer using forecast data) 
+    df = df[df.index <= latest_rain_time]
+
     min_time = min(df.index)
     max_time = max(df.index)
 
-    rng = pd.date_range(min_time, max_time, freq='15Min')
+    # Fill in missing timestamps by reindexing
+    rng = pd.date_range(min_time, max_time + pd.Timedelta('2.5hours'), freq='15Min')
     df = df.reindex(rng)
 
-    logger.debug("Convert cumulative rain to actual rain")
-
+    # Convert cumulative rain to actual rain
     df['rain'] = df['cum_rain'].diff(periods=2)
 
     # negative values from diff are when the rain value resets so we set equal to the cumulative value
     df.loc[df['rain'] < 0, 'rain'] = df.loc[df['rain'] < 0, 'cum_rain']
 
-    
-    latest_rain_time = max(df.index[df.cum_rain.notnull()])
-    logger.info('latest rain update at: ' + str(latest_rain_time))
+    df['model_rain'] = df["rain"]
 
-    logger.debug("Concat rain and forecast to create model_rain")
-
-    df['model_rain'] = pd.concat([
-        df[df.index <= latest_rain_time]['rain'],
-        df[df.index > latest_rain_time]['forecast']
-    ])
-
-    logger.debug("interpolate model_rain")
+    # Interpolate model_rain
 
     df['model_rain'] = df['model_rain'].interpolate()
 
-    return df
+    input_df = pd.concat((
+        df[df.index <=latest_level_update_timestamp].tail(num_level_updates),
+        df[df.index >latest_level_update_timestamp].tail(num_level_updates)
+    ))
 
+    x = input_df.model_rain.values         
+    y = input_df.level.fillna(0).values
+    timestamps = input_df.index.values
 
-def pre_model_checks(df, current_time):
-    # Check that there is a level update in df
-    if len(df[df.level.notnull()]) == 0:
-        logger.warning("No level update - exiting")
-        sys.exit()   
-    # Check that there is a row for now or past now
-    if len(df[df.index >= current_time]) == 0:
-        logger.warning("Not enough data - exiting")
-        sys.exit()
+    # need to padd out input arrays with zeros to get the correct shape
+    num_padding_steps = num_steps - x.shape[0] 
+    x = np.concatenate((x, np.zeros(num_padding_steps)))
+    y = np.concatenate((y, np.zeros(num_padding_steps)))   
 
-        
-def model(df, from_latest_level=True):
-    logger.info('*** RUNNING MODEL ***')
-    if from_latest_level:
-        # for running model from latest level update onwards
-        starting_time = max(df.index[df.level.notnull()])
+    update_vector = np.zeros(x.shape)
+    update_vector[0:num_level_updates] = 1
+
+    x = np.column_stack([x, update_vector, update_vector*y])
+    y = np.column_stack([y])
+
+    model_name = "production_rnn"
+    path_to_model = os.path.join(FDIR, model_name)
+    predict_fn = tf.contrib.predictor.from_saved_model(path_to_model)
+    predict = predict_fn({"x":[x]})["predictions"]
+
+    # remove excess padding
+    predict = predict[:-num_padding_steps]
+    level = y[:-num_padding_steps,0]
+    rain = x[:-num_padding_steps,0]
+
+    # set levels after latest update and predicts before to None
+    level[num_level_updates:] = None
+    predict[:num_level_updates-1] = None
+
+    # create output json
+    output_df = pd.DataFrame({"timestamp":timestamps, "rain":rain, "level":level, "predict": predict})
+
+    output_df = output_df.round({'level': 3, 'predict': 3, 'model_rain' : 1})
+    output_df = pd.DataFrame(output_df).replace({np.nan:None})
+
+    if latest_level_update_timestamp == current_time:
+        current_level = output_df[output_df.timestamp == current_time]["level"].values[0]
     else:
-        # for running model on entire dataframe
-        starting_time = df.index[df.level.notnull()][5]
-    starting_level = df.loc[starting_time].level
+        current_level = output_df[output_df.timestamp == current_time]["predict"].values[0]
 
-
-    logger.info('Run model from: ' + str(starting_time))
-    logger.info('Starting level update: ' + str(starting_level))
-
-
-    df['storage'] = np.nan
-    df['predict'] = np.nan
-
-    # Calculate initial storage
-    init_storage = f_inv(g_inv(starting_level))
-    df.loc[starting_time, 'storage'] = init_storage
-    storage = init_storage
-
-    # Run iteration for indexes > latest_level_update
-    df_model = df[(df.index > pd.Timestamp(starting_time))]
-
-    for i,r in df_model.iterrows():
-        rain = df.loc[i - delay, 'model_rain']
-        predict = g(f(storage))
-        storage = storage + rain - f(storage)
-        df.loc[i, 'storage'] = storage
-        df.loc[i, 'predict'] = predict
-
-    return df
-
-
-# # Create export dictionary
-# 
-# * Round model_rain, level and predict
-# * Get current time rounded down to nearest 15 minutes
-# * create output dict with the following properties
-#     * values
-#     * current_time
-#     * current_level
-#     * text
-#     * next_up if in next hour
-
-def model_export(df, current_time):
-    # Round export columns
-    df = df.round({'level': 3, 'predict': 3, 'model_rain' : 1})
-
-    try:
-        current_row = df.loc[pd.to_datetime(current_time, unit='s')]
-        current_level = current_row['level']
-        if np.isnan(current_level):
-            current_level = current_row['predict']
-    except KeyError:
-        print "Can't find row in df that matches current time: "+ time.strftime(time_format, time.gmtime(current_time))
-        current_level = None
-
-    
     logger.info('currenct level: ' + str(current_level))
 
-    df.timestamp = df.index
-    df = df.where((pd.notnull(df)), None)
-    timestamp_vals = [timestmp.value / 1000 for timestmp in df.index.tolist()]
-    rain_vals = df.model_rain.tolist()
-    level_vals = df.level.tolist()
-    predict_vals = df.predict.tolist()
-    values = []
-    for n in range(0, len(timestamp_vals)):
-        values.append({'timestamp' : timestamp_vals[n], 'rain' : rain_vals[n], 'level' : level_vals[n], 'predict' : predict_vals[n]})
+    minimum_threshold = 0.7
+    massive_threshold = 1.5
 
-    if current_level > 1.5:
+    if current_level > massive_threshold:
         text = "THE DART IS MASSIVE"
-    elif current_level > 0.7:
+    elif current_level > minimum_threshold:
         text = 'YES'
+    elif output_df[output_df.timestamp > current_time]["predict"].max() > minimum_threshold:
+        text = "THE DART WILL BE UP SHORTLY"
     else:
-        next_up = df[(df.index > current_time) & (df.index < current_time + delay) & (df.predict > 0.7)].index.min()
-        if pd.isnull(next_up):
-            text = 'NO'
-        else:
-            text = "THE DART WILL BE UP SHORTLY"    
-    
+        text = 'NO'    
+
     logger.info("OUTPUT TEXT: " + text)
 
-
+    output_df.timestamp = [timestamp.value / 1000 for timestamp in output_df.timestamp.tolist()]
+    values = output_df.T.to_dict().values()
 
     output = {}       
     output['current_time'] = current_time.value / 1000
-    output['current_level'] = current_level 
+    output['current_level'] = float(current_level) 
     output['text'] = text
     output['values'] = values
-
-
     return output
-# # Write export to json
-
-def fb_cache_update():
-    from local_info import facebook_access 
-    r = requests.post("https://graph.facebook.com", data={'scrape': 'True', 'id' : '  http://isthedartrunning.co.uk/', 'access_token' : facebook_access})
-
-def upload_export(testing, output):
-    """Upload json file to webpage via ftp and then force fb to update cache"""
-    filename = "dart.json"
-    with open(os.path.join(FDIR, '../' + filename), 'w') as f:
-        json.dump(output, f, indent=4)
-
-    from local_info import ftp_url, ftp_pass, ftp_user, ftp_dir
-    ftp = ftplib.FTP(ftp_url)
-    ftp.login(ftp_user, ftp_pass)
-    if ftp_dir is not None:
-        ftp.cwd(ftp_dir)
-
-    ftp.storbinary("STOR " + filename, open(os.path.join(FDIR, '../' + filename)), 1024)
-    fb_cache_update()
-
-
 
 def upload_export_s3(testing, output):
     from local_info import aws_access_key_id, aws_secret_access_key, region_name, bucket_name
@@ -235,13 +176,13 @@ def upload_export_s3(testing, output):
     s3 = session.resource('s3')
     bucket = s3.Bucket(bucket_name)
     filename="dart.json"
+    print output
     with open(os.path.join(FDIR, '../' + filename), 'w') as f:
         json.dump(output, f, indent=4)
     
     with open(os.path.join(FDIR, '../' + filename)) as data:
         bucket.put_object(Key=filename, Body=data, ContentType="text/json")
 
-    fb_cache_update()
 
 def run(testing):
     start_time = time.time()
@@ -250,24 +191,7 @@ def run(testing):
 
     df = load_dataframe_from_sql(river="dart", limit=130)
 
-    df = preprocessing(df)
-
-    # # Calculate important timestamps
-    
-    # current time rounded down to nearest 15 minutes
-    current_time = time.time()
-    current_time = pd.to_datetime(current_time - (current_time % (15*60)), unit='s')
-    logger.info('Current_time: ' + str(current_time))
-    
-
-    # # Pre-model checks
-    pre_model_checks(df, current_time)
-
-    # run model
-    df = model(df)
-
-    # create export
-    output = model_export(df, current_time)
+    output = rnn_model(df)    
 
     # upload export
     #upload_export(testing, output)
