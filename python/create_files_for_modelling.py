d400aa02840581bd85b0b35c23cc511391d74abb
Jamie Conn <jamie.conn65@gmail.com>
2020-06-07 17:21:02 +0100

    Change the output data for modelling

--- python/create_files_for_modelling.py
+++ python/create_files_for_modelling.py
@@ -17,17 +17,26 @@ rng = pd.date_range(min_time, max_time, freq='15Min')
 df = df.reindex(rng)
 df.level = df.level.fillna(method="pad")
 
+
+df['rain'] = df['cum_rain'].diff(periods=2)
+# negative values from diff are when the rain value resets so we set equal to the cumulative value
+df.loc[df['rain'] < 0, 'rain'] = df.loc[df['rain'] < 0, 'cum_rain']
+#interpolate the rain values
+
+df['rain'] = df['rain'].interpolate()
+df['rain'] = df['rain'].fillna(0)
+
 df["image_filenames"] = df.index.strftime('%Y-%m-%dT%H:%M:00.png')
 
-train_df = df.iloc[:int(df.shape[0]/5),:]
-test_df = df.iloc[int(df.shape[0]/5):3*int(df.shape[0]/10),:]
+train_df = df.iloc[:int(df.shape[0]/2),:]
+test_df = df.iloc[int(df.shape[0]/2):,:]
 train_df.to_csv("train.csv")
 test_df.to_csv("test.csv")
 
 f = h5py.File('images.h5', 'w')
 start = time.time()
 
-images = np.array([], dtype=np.uint8).reshape(0, 250, 250)
+images = np.array([], dtype=np.uint8).reshape(0, 12, 12)
 for i, filename in enumerate(train_df.image_filenames.values):
   if i % 100 == 0:
     print i
@@ -38,11 +47,11 @@ for i, filename in enumerate(train_df.image_filenames.values):
   except:
     print "failed to read" + filename
     flattened_image = np.zeros((500, 500), dtype=np.uint8) 
-  images = np.concatenate((images, np.expand_dims(flattened_image[250:,:250], 0)))
+  images = np.concatenate((images, np.expand_dims(flattened_image[:,396:408, 230:242], 0)))
 
 
 f.create_dataset('train', data=images, dtype="uint8")
-images = np.array([], dtype=np.uint8).reshape(0, 250, 250)
+images = np.array([], dtype=np.uint8).reshape(0, 12, 12)
 for i, filename in enumerate(test_df.image_filenames.values):
   if i % 100 == 0:
     print i
@@ -53,7 +62,7 @@ for i, filename in enumerate(test_df.image_filenames.values):
   except:
     print "failed to read" + filename
     flattened_image = np.zeros((500, 500), dtype=np.uint8) 
-  images = np.concatenate((images, np.expand_dims(flattened_image[250:,:250], 0)))
+  images = np.concatenate((images, np.expand_dims(flattened_image[:,396:408, 230:242], 0)))
 
 f.create_dataset('test', data=images, dtype="uint8")
 
