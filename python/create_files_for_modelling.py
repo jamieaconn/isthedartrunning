d7cdcb97e2bc197bb2dfaf9df6adeae24c8515f2
root <jamie.conn65@gmail.com>
2021-01-07 18:29:42 +0000

    Upload modelling files to s3

--- python/create_files_for_modelling.py
+++ python/create_files_for_modelling.py
@@ -6,6 +6,20 @@ import time
 import h5py
 import numpy as np
 import pandas as pd
+import boto3
+from io import BytesIO
+from local_info import aws_access_key_id, aws_secret_access_key, region_name, bucket_name
+import PIL
+
+session = boto3.Session(
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    region_name=region_name
+)
+
+s3 = session.resource('s3')
+bucket = s3.Bucket(bucket_name)
+
 
 start_date='2017-12-10 16:15:00'
 end_date='2019-04-14 11:45:0'
@@ -30,39 +44,75 @@ df["image_filenames"] = df.index.strftime('%Y-%m-%dT%H:%M:00.png')
 
 train_df = df.iloc[:int(df.shape[0]/2),:]
 test_df = df.iloc[int(df.shape[0]/2):,:]
-train_df.to_csv("train.csv")
-test_df.to_csv("test.csv")
 
-f = h5py.File('images.h5', 'w')
+
+csv_buffer = BytesIO()
+train_df.to_csv(csv_buffer)
+bucket.put_object(Key='modelling_data/train.csv', Body=csv_buffer.getvalue())
+
+csv_buffer = BytesIO()
+test_df.to_csv(csv_buffer)
+bucket.put_object(Key='modelling_data/test.csv', Body=csv_buffer.getvalue())
+
 start = time.time()
 
-images = np.array([], dtype=np.uint8).reshape(0, 100, 100)
-for i, filename in enumerate(train_df.image_filenames.values):
-  if i % 100 == 0:
-    print i
-    print time.time() - start
-  try:
-    image = imageio.imread('../image/radar/' + filename)
-    flattened_image = image_processing.flatten_radar_image(image)   
-  except:
-    print "failed to read" + filename
-    flattened_image = np.zeros((500, 500), dtype=np.uint8) 
-  images = np.concatenate((images, np.expand_dims(flattened_image[376:476, 210:310], 0)))
+chunks = list()
+chunk_size = 2000
+num_chunks = len(train_df) // chunk_size + 1
+for i in range(num_chunks):
+  chunks.append(train_df[i*chunk_size:(i+1)*chunk_size])
+
+for j, chunk in enumerate(chunks):
+  filepath = 'modelling_data/train_images_' + str(j) + '.h5'
+  uploadpath = 'modelling_data/train_images_' + str(j) + '.h5'
+  f = h5py.File(filepath, 'w')
+  print "Training data - Chunk: " + str(j) + "/" + str(num_chunks)
+  images = np.array([], dtype=np.uint8).reshape(0, 500, 500)
+  for i, filename in enumerate(chunk.image_filenames.values):
+    if i % 100 == 0:
+      print i
+      print time.time() - start
+    try:
+      image = imageio.imread('../image/radar/' + filename)
+      flattened_image = image_processing.flatten_radar_image(image)   
+    except:
+      print "failed to read" + filename
+      flattened_image = np.zeros((500, 500), dtype=np.uint8) 
+    images = np.concatenate((images, np.expand_dims(flattened_image, 0)))
+
+  f.create_dataset('images', data=images, dtype="uint8")
+  f.close()
+  print(filepath)
+  with open(filepath) as data:
+    bucket.put_object(Key=uploadpath, Body=data)
 
 
-f.create_dataset('train', data=images, dtype="uint8")
-images = np.array([], dtype=np.uint8).reshape(0, 100, 100)
-for i, filename in enumerate(test_df.image_filenames.values):
-  if i % 100 == 0:
-    print i
-    print time.time() - start
-  try:
-    image = imageio.imread('../image/radar/' + filename)
-    flattened_image = image_processing.flatten_radar_image(image)   
-  except:
-    print "failed to read" + filename
-    flattened_image = np.zeros((500, 500), dtype=np.uint8) 
-  images = np.concatenate((images, np.expand_dims(flattened_image[376:476, 210:310], 0)))
+chunks = list()
+chunk_size = 2000
+num_chunks = len(test_df) // chunk_size + 1
+for i in range(num_chunks):
+  chunks.append(test_df[i*chunk_size:(i+1)*chunk_size])
 
-f.create_dataset('test', data=images, dtype="uint8")
+for j, chunk in enumerate(chunks):
+  filepath = 'modelling_data/test_images_' + str(j) + '.h5'
+  uploadpath = 'modelling_data/test_images_' + str(j) + '.h5'
+  f = h5py.File(filepath, 'w')
+  print "Test data - Chunk: " + str(j) + "/" + str(num_chunks)
+  images = np.array([], dtype=np.uint8).reshape(0, 500, 500)
+  for i, filename in enumerate(chunk.image_filenames.values):
+    if i % 100 == 0:
+      print i
+      print time.time() - start
+    try:
+      image = imageio.imread('../image/radar/' + filename)
+      flattened_image = image_processing.flatten_radar_image(image)   
+    except:
+      print "failed to read" + filename
+      flattened_image = np.zeros((500, 500), dtype=np.uint8) 
+    images = np.concatenate((images, np.expand_dims(flattened_image, 0)))
 
+  f.create_dataset('images', data=images, dtype="uint8")
+  f.close()
+  print(filepath)
+  with open(filepath) as data:
+    bucket.put_object(Key=uploadpath, Body=data)
