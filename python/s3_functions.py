558e3b8e7223b9c91445840e9c4eb633b6625189
Jamie Conn <jamie.conn@sparx.co.uk>
2023-11-07 08:43:00 +0000

    Fix up s3 functions

--- python/s3_functions.py
+++ python/s3_functions.py
@@ -1,18 +1,11 @@
 import boto3
 from local_info import aws_access_key_id, aws_secret_access_key, region_name, bucket_name
 import os
-import modelLib
 import imageio
 import time
 import image_processing
 
-session = boto3.Session(
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key,
-    region_name=region_name
-)
-s3 = session.resource('s3')
-bucket = s3.Bucket(bucket_name)
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
 RIVER_NAME = "dart"
 OUTPUT_FILENAME = RIVER_NAME + ".json"
@@ -21,44 +14,48 @@ OUTPUT_PATH = os.path.join(FDIR, '../html', OUTPUT_FILENAME)
 
 def upload_to_s3(filepath, uploadpath):
     with open(filepath) as data:
-        bucket.put_object(Key=uploadpath, Body=data, ContentType="text/json")
+        bucket.put_object(Key=uploadpath, Body=data, ContentType="image/png")
 
 def upload_radar_images_to_s3():
   dir_path = "../image/radar"
   for f in os.listdir(dir_path):
     upload_to_s3(os.path.join(dir_path, f), os.path.join('images', f))
 
-def upload_sql_to_s3_as_csv():
-  df = modelLib.load_dataframe_from_sql("dart", limit=-1)
-  df.to_csv("../data.csv")
-  upload_to_s3("../data.csv", "data.csv")
   
 
 def list_radar_images_in_s3():
-  existing_image_filenames = [obj.key[len("images/"):] for obj in bucket.objects.filter(Prefix="images/")]
-  return(existing_image_filenames)
+  response = s3.list_objects_v2(Bucket=bucket_name, Prefix="images/")
+
+  # Extract the list of files in the directory
+  files = [obj['Key'] for obj in response.get('Contents', [])]
+  # remove /images
+  files = [f[7:] for f in files]
+  return(files)
 
 
 def flatten_and_upload_all_radar_images_s3():
     start = time.time()
-    filenames = os.listdir('../image/radar')
-    existing_image_filenames = list_radar_images_in_s3()
+    filenames = os.listdir('/mnt/new/root/operational/isthedartrunning/image/radar/')
+    timestamps = [filename.split('.')[0] for filename in filenames]
 
-    new_filenames = [f for f in filenames if f not in existing_image_filenames]
+    existing_filenames = list_radar_images_in_s3()
+    existing_timestamps = [filename.split('.')[0] for filename in existing_filenames]
 
-    len(new_filenames)
-    for i, filename in enumerate(new_filenames):
+    new_timestamps = [t for t in timestamps if t not in existing_timestamps]
+    print(len(timestamps))
+    print(len(existing_timestamps))
+    print(len(new_timestamps))
+    for i, timestamp in enumerate(new_timestamps):
       if i % 100 == 0:
         print(i)
         print(time.time() - start)
       try:
-        image = imageio.imread('../image/radar/' + filename)
+        image = imageio.imread('/mnt/new/root/operational/isthedartrunning/image/radar/' + timestamp + ".png")
       except:
         print("failed to read" + filename)
         continue
-      flattened_image = image_processing.flatten_radar_image(image)
-      imageio.imwrite('temp.png', flattened_image)
-      upload_to_s3('temp.png', 'images/'+filename)
+      array_bytes = image_processing.flatten_radar_image(image).tobytes()
+      s3.put_object(Bucket=bucket_name, Key="images/"+timestamp + ".npz", Body=array_bytes)
 
     print(time.time() - start)
 
@@ -66,7 +63,7 @@ def flatten_and_upload_all_radar_images_s3():
 
 image_file_extensions = [".jpg", ".png"]
 
-def upload_static_files(bucket=bucket, path='../html', ignore_images=True):
+def upload_static_files(bucket, path='../html', ignore_images=True):
     for subdir, dirs, files in os.walk(path):
         for file in files:
             full_path = os.path.join(subdir, file)
@@ -87,7 +84,7 @@ def upload_static_files(bucket=bucket, path='../html', ignore_images=True):
                 else:
                     bucket.put_object(Key=full_path[len(path)+1:], Body=data)
  
-def delete_static_files(bucket=bucket, ignore_images=False):
+def delete_static_files(bucket, ignore_images=False):
     keys = bucket.objects.all()
     for key in keys:
         print(key)
